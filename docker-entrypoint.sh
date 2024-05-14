a05ec0e342338284fb4959b37c5f6200bf0e3b15
moefaq <124541328+moefaq@users.noreply.github.com>
2024-05-14 21:10:53 +0800

    Update: docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 
-conver_to_array(){
-    local BOT_SEND_ID_env=$1
-    local IFS=","
-    str=""
-    for send_id in ${BOT_SEND_ID_env};do
-        str="$str    - ${send_id}\n"
-    done
-    result=`echo -e "${str}"`
-}
-AUTOREPLY=`echo -e "${AUTOREPLY}"`
+# conver_to_array(){
+#     local BOT_SEND_ID_env=$1
+#     local IFS=","
+#     str=""
+#     for send_id in ${BOT_SEND_ID_env};do
+#         str="$str    - ${send_id}\n"
+#     done
+#     result=`echo -e "${str}"`
+# }
+# AUTOREPLY=`echo -e "${AUTOREPLY}"`
 
 if [ ! -e "/Crisp-Telegram-Bot/config.yml" ]; then
-    conver_to_array ${BOT_SEND_ID}
+    # conver_to_array ${BOT_SEND_ID}
     cat > /Crisp-Telegram-Bot/config.yml << EOF
 bot:
   token: ${BOT_TOKEN}
-  send_id:
-${result}
+  groupId: ${BOT_GROUPID}
 crisp:
   id: ${CRISP_ID}
   key: ${CRISP_KEY}
   website: ${CRISP_WEBSITE}
-autoreply:
-${AUTOREPLY}
 EOF
 fi
 exec "$@"
